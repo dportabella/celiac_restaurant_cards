d383535ccaec0ce18c3e601304fd4d93a5215e93
David Portabella <david.portabella@gmail.com>
2014-04-19 22:10:44 +0200

    some example files, and reading them

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -1,3 +1,7 @@
 object Hi {
-  def main(args: Array[String]) = println("Hi!")
+  def main(args: Array[String]) = {
+    println("Hi!")
+//    println(scala.io.Source.fromFile("src/resources/english.fo").getLines.mkString("\n"))
+    println(scala.io.Source.fromInputStream(getClass.getResourceAsStream("english.fo")).getLines.mkString("\n"))
+  }
 }
